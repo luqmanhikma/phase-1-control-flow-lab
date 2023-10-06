3a7e95c478d43643b880cad8a01b6d78cc34d859
luqman <luqmanito602@gmail.com>
2023-10-06 09:17:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <='400')
+  return 'This one is on me!';
+else if (ride >2000 && ride <= 2500)
+return 'I will gladly take your thirty bucks.';
+else (ride > 2500)
+return 'No can do.';
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  if (city !== 'NYC')
+   return 'No go.';
+  else return 'Ok, sounds good.';
 
-function switchOnCharmFromTip(){
+
+ 
+}
   // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+switch (tip){
+  case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous':
+   return 'Thank you.';
+  default: 'thanks for everything'
+  return 'Bye.';
+}
 }
